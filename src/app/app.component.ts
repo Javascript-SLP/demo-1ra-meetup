c6bff5904b39e8971cc73a64600b237874577e94
Luis Medellin <medelling24@gmail.com>
2018-02-16 09:48:46 -0600

    FB login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
+import { FacebookService, InitParams, LoginResponse } from 'ngx-facebook';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements AfterContentInit{
+  title = 'Login con FB';
+  private name: string;
+  private pic_url: string;
+  private login: boolean;
+
+  constructor(private fb: FacebookService) {
+    this.login = false;
+  }
+
+  ngAfterContentInit() {
+    const initParams: InitParams = {
+      appId: '1236516716401897',
+      xfbml: true,
+      version: 'v2.8'
+    };
+
+    this.fb.init(initParams);
+  }
+
+  loginWithFB(): void {
+    this.fb.login()
+      .then((res: LoginResponse) => {
+        this.login = true;
+        console.log('Logged in', res);
+        this.getProfile();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  getProfile() {
+    this.fb.api('/me?fields=picture,name')
+      .then((res: any) => {
+        console.log('Got the users profile', res);
+        this.name = res.name;
+        this.pic_url = res.picture.data.url;
+      })
+      .catch((error: any) => console.error(error));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { FacebookModule } from 'ngx-facebook';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FacebookModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
